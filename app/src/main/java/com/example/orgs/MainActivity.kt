08bc6fbc84c9dc65bcf7d004a7cee2e8e2a3b5c2
Júlia Conifer <julia.conip@gmail.com>
2024-06-05 21:20:58 -0300

    02. Criando o primeiro layout da Activity

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.orgs
 
 import android.app.Activity
+import android.os.*
 
-class MainActivity : Activity()
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
